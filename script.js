6aa25cdbe89156d44361c9aecdf47ccf46cc48c0
ysbllcby <ysabellacubay@gmail.com>
2023-05-07 13:58:24 -0600

    Connected to API.

---
+++ script.js
@@ -0,0 +1,33 @@
+let weather = {
+  apiKey: "1df9522859971edb3361be0cb099aa19",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+
+  // Function to display weather
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+
+    console.log(name, icon, description, temp, humidity, speed);
+
+    // Display in HTML
+    document.querySelector(".city").innerHTML = "Weather in " + name;
+    document.querySelector(".temp").innerHTML = temp + "°C";
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerHTML = description;
+    document.querySelector(".humidity").innerHTML =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerHTML = "Windspeed: " + speed + " km/h";
+  },
+};
